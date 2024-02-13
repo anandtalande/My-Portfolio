5d05bcce8d4f3030fc6d8f12e5350cf9abf9816d
anandtalande <anandtalande01@gmail.com>
2024-02-13 14:25:01 +0530

    done

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -3,28 +3,25 @@ import ProjectCard from "../sub/ProjectCard";
 
 const Projects = () => {
   return (
-    <div
-      className="flex flex-col items-center justify-center py-20"
-      id="projects"
-    >
-      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+    <div className="flex flex-col items-center justify-center py-20" id="projects">
+      <h1 className="text-4xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-10">
         My Projects
       </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-10 px-10">
         <ProjectCard
-          src="/1.png"
           title="Fearless - Women's Safety"
           description="This innovative Android application is designed with a sole focus on women's security, offering a range of features to keep you safe, connected, and in control."
+          images={["/1.png", "/2.png", "/3.png", "/4.png"]}
         />
         <ProjectCard
-          src="/Screenshot 3.png"
           title="Personal Portfolio"
-          description="Experience my portfolio, a synergy of modern technology and elegant design.  A commitment to innovation and user-centric design principles."
+          description="Experience my portfolio, a synergy of modern technology and elegant design. A commitment to innovation and user-centric design principles."
+          images={["/Screenshot 3.png"]}
         />
         <ProjectCard
-          src="/3.png"
-          title="Personal Portfolio"
-          description="Experience my portfolio, a synergy of modern technology and elegant design.  A commitment to innovation and user-centric design principles."
+          title="Another Project"
+          description="Description for another project."
+          images={["/3.png"]}
         />
       </div>
     </div>
--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -1,23 +1,47 @@
+'use client'
+// Import the necessary modules and styles
 import Image from "next/image";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+// Define the Props interface
 interface Props {
-  src: string;
   title: string;
   description: string;
+  images: string[];
 }
 
-const ProjectCard = ({ src, title, description }: Props) => {
+// ProjectCard component
+const ProjectCard = ({ title, description, images }: Props) => {
+  // State to track the current image index
+  const [currentImage, setCurrentImage] = useState(0);
+
+  // useEffect to handle image change at intervals
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+
+    intervalId = setInterval(() => {
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    }, 2000);
+
+    // Cleanup function to clear the interval on component unmount
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [images]);
+
+  // Render the component
   return (
-    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
+    <div className="relative overflow-hidden rounded-lg shadow-lg border transition-opacity duration-500 ease-in-out transform hover:scale-110 border-dot">
+      {/* Image component */}
       <Image
-        src={src}
+        src={images[currentImage]}
         alt={title}
         width={1000}
         height={1000}
         className="w-full object-contain"
       />
 
+      {/* Project information */}
       <div className="relative p-4">
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
         <p className="mt-2 text-gray-300">{description}</p>
